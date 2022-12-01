6798a24713daacd3b119b6eff44de230053b1bad
Takuya Murakami <tmurakam@tmurakam.org>
2022-12-02 00:48:10 +0900

    add: playsound

--- alerter.py
+++ alerter.py
@@ -1,10 +1,16 @@
 # Alerter
+
+from playsound import playsound
+
 class Alerter:
     def go_hot(self):
         print("==> Go HOT")
+        playsound("sound/GoHot.mp3")
 
     def go_cold(self):
         print("==> Go COLD")
+        playsound("sound/GoCold.mp3")
 
     def hot_alarm(self, duration):
         print("==> HOT ALARM:", duration, "sec")
+        playsound("sound/HotAlarm.mp3")
--- main.py
+++ main.py
@@ -2,18 +2,17 @@
 # Stove temperature watcher with thermal sensor
 
 from Adafruit_AMG88xx import Adafruit_AMG88xx
-import thermal_display
-import stove_watcher as w
-import alerter
+from thermal_display import ThermalDisplay
+from stove_watcher import StoveWatcher
+from alerter import Alerter
 
 # initialize the sensor
 sensor = Adafruit_AMG88xx()
 
-# initialize display
-display = thermal_display.ThermalDisplay()
-
-alerter = alerter.Alerter()
-watcher = w.StoveWatcher(alerter)
+# initialize
+display = ThermalDisplay()
+alerter = Alerter()
+watcher = StoveWatcher(alerter)
 
 # main loop
 while 1:
