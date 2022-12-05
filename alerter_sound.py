fd2a45254fce9d4170eb5d5e668c813d0e774950
Takuya Murakami <tmurakam@tmurakam.org>
2022-12-05 18:57:53 +0900

    feat: add logging

--- alerter_sound.py
+++ alerter_sound.py
@@ -1,10 +1,13 @@
 # Alerter
 
 import os
+from logging import getLogger
 
 from playsound import playsound
 
 DEVICE = "Living Echo"
+
+log = getLogger(__name__)
 
 
 class Alerter:
@@ -15,18 +18,22 @@ class Alerter:
         print("==> Started")
         playsound("sound/GoHot.mp3")
         self.alexa("ガスコンロの監視を開始しました")
+        log.info("Start watching")
 
     def go_hot(self):
-        print("==> Go HOT")
+        print("==> Got HOT")
         playsound("sound/GoHot.mp3")
         self.alexa("ガスコンロの温度が上がりました")
+        log.info("Got HOT")
 
     def go_cold(self):
-        print("==> Go COLD")
+        print("==> Be COLD")
         playsound("sound/GoCold.mp3")
         self.alexa("ガスコンロの温度が下がりました")
+        log.info("Got COLD")
 
     def hot_alarm(self, duration):
         print("==> HOT ALARM:", duration, "sec")
         playsound("sound/HotAlarm.mp3")
         self.alexa("ガスコンロの温度が上がったままです")
+        log.info("Alarm HOT")
--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
 # Stove temperature watcher with thermal sensor
 import time
+import logging
 
 from Adafruit_AMG88xx import Adafruit_AMG88xx
 from thermal_display import ThermalDisplay
 from stove_watcher import StoveWatcher
 from alerter_sound import Alerter
 #from alerter_alexa import Alerter
+
+# initialize logging
+formatter = '%(asctime)s : %(message)s'
+logging.basicConfig(filename="stove_watcher.log", format=formatter, level=logging.INFO, datefmt="%Y/%m/%d %H:%M:%S")
 
 # initialize the sensor
 sensor = Adafruit_AMG88xx(address=0x68)
